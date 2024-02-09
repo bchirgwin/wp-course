40dd891f4c367f7183a274fca22681048499c14c
Brian Chirgwin <bchirgwin42@gmail.com>
2024-02-09 10:40:34 -0600

    create constants for player1 and player2

--- homework/refactor-assignent-solution/src/index.js
+++ homework/refactor-assignent-solution/src/index.js
@@ -1,35 +1,38 @@
 'use strict';
 
-function isWinner(board, c) {
-  if (board[0][0] === c && board[0][1] === c && board[0][2] === c) {
+const PLAYER1 = 1;
+const PLAYER2 = 2;
+
+function isWinner(board, player) {
+  if (board[0][0] === player && board[0][1] === player && board[0][2] === player) {
     return true;
   }
 
-  if (board[1][0] === c && board[1][1] === c && board[1][2] === c) {
+  if (board[1][0] === player && board[1][1] === player && board[1][2] === player) {
     return true;
   }
 
-  if (board[2][0] === c && board[2][1] === c && board[2][2] === c) {
+  if (board[2][0] === player && board[2][1] === player && board[2][2] === player) {
     return true;
   }
 
-  if (board[0][0] === c && board[1][0] === c && board[2][0] === c) {
+  if (board[0][0] === player && board[1][0] === player && board[2][0] === player) {
     return true;
   }
 
-  if (board[0][1] === c && board[1][1] === c && board[2][1] === c) {
+  if (board[0][1] === player && board[1][1] === player && board[2][1] === player) {
     return true;
   }
 
-  if (board[0][2] === c && board[1][2] === c && board[2][2] === c) {
+  if (board[0][2] === player && board[1][2] === player && board[2][2] === player) {
     return true;
   }
 
-  if (board[0][0] === c && board[1][1] === c && board[2][2] === c) {
+  if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
     return true;
   }
 
-  if (board[0][2] === c && board[1][1] === c && board[2][0] === c) {
+  if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
     return true;
   }
 
@@ -39,11 +42,11 @@ function isWinner(board, c) {
 console.log(
   isWinner(
     [
-      [1, 1, 1],
+      [PLAYER1, PLAYER1, PLAYER1],
       [0, 0, 0],
       [0, 0, 0],
     ],
-    1
+    PLAYER1
   )
 );
 
@@ -51,98 +54,10 @@ console.log(
   isWinner(
     [
       [0, 0, 0],
-      [1, 1, 1],
+      [PLAYER1, PLAYER1, PLAYER1],
       [0, 0, 0],
     ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [0, 0, 0],
-      [0, 0, 0],
-      [1, 1, 1],
-    ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [1, 0, 0],
-      [1, 0, 0],
-      [1, 0, 0],
-    ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [0, 1, 0],
-      [0, 1, 0],
-      [0, 1, 0],
-    ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [0, 0, 1],
-      [0, 0, 1],
-      [0, 0, 1],
-    ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [1, 0, 0],
-      [0, 1, 0],
-      [0, 0, 1],
-    ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [0, 0, 1],
-      [0, 1, 0],
-      [1, 0, 0],
-    ],
-    1
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [2, 2, 2],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-    2
-  )
-);
-
-console.log(
-  isWinner(
-    [
-      [0, 0, 0],
-      [2, 2, 2],
-      [0, 0, 0],
-    ],
-    2
+    PLAYER1
   )
 );
 
@@ -151,85 +66,173 @@ console.log(
     [
       [0, 0, 0],
       [0, 0, 0],
-      [2, 2, 2],
+      [PLAYER1, PLAYER1, PLAYER1],
     ],
-    2
+    PLAYER1
   )
 );
 
 console.log(
   isWinner(
     [
-      [2, 0, 0],
-      [2, 0, 0],
-      [2, 0, 0],
+      [PLAYER1, 0, 0],
+      [PLAYER1, 0, 0],
+      [PLAYER1, 0, 0],
     ],
-    2
+    PLAYER1
   )
 );
 
 console.log(
   isWinner(
     [
-      [0, 2, 0],
-      [0, 2, 0],
-      [0, 2, 0],
+      [0, PLAYER1, 0],
+      [0, PLAYER1, 0],
+      [0, PLAYER1, 0],
     ],
-    2
+    PLAYER1
   )
 );
 
 console.log(
   isWinner(
     [
-      [0, 0, 2],
-      [0, 0, 2],
-      [0, 0, 2],
+      [0, 0, PLAYER1],
+      [0, 0, PLAYER1],
+      [0, 0, PLAYER1],
     ],
-    2
+    PLAYER1
   )
 );
 
 console.log(
   isWinner(
     [
-      [2, 0, 0],
-      [0, 2, 0],
-      [0, 0, 2],
+      [PLAYER1, 0, 0],
+      [0, PLAYER1, 0],
+      [0, 0, PLAYER1],
     ],
-    2
+    PLAYER1
   )
 );
 
 console.log(
   isWinner(
     [
-      [0, 0, 2],
-      [0, 2, 0],
-      [2, 0, 0],
+      [0, 0, PLAYER1],
+      [0, PLAYER1, 0],
+      [PLAYER1, 0, 0],
     ],
-    2
+    PLAYER1
   )
 );
 
 console.log(
   isWinner(
     [
-      [1, 2, 1],
-      [1, 2, 2],
-      [2, 1, 1],
+      [PLAYER2, PLAYER2, PLAYER2],
+      [0, 0, 0],
+      [0, 0, 0],
     ],
-    1
+    PLAYER2
   )
 );
 
 console.log(
   isWinner(
     [
-      [1, 2, 1],
-      [1, 2, 2],
-      [2, 1, 1],
+      [0, 0, 0],
+      [PLAYER2, PLAYER2, PLAYER2],
+      [0, 0, 0],
     ],
-    2
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [0, 0, 0],
+      [0, 0, 0],
+      [PLAYER2, PLAYER2, PLAYER2],
+    ],
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [PLAYER2, 0, 0],
+      [PLAYER2, 0, 0],
+      [PLAYER2, 0, 0],
+    ],
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [0, PLAYER2, 0],
+      [0, PLAYER2, 0],
+      [0, PLAYER2, 0],
+    ],
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [0, 0, PLAYER2],
+      [0, 0, PLAYER2],
+      [0, 0, PLAYER2],
+    ],
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [PLAYER2, 0, 0],
+      [0, PLAYER2, 0],
+      [0, 0, PLAYER2],
+    ],
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [0, 0, PLAYER2],
+      [0, PLAYER2, 0],
+      [PLAYER2, 0, 0],
+    ],
+    PLAYER2
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [PLAYER1, PLAYER2, PLAYER1],
+      [PLAYER1, PLAYER2, PLAYER2],
+      [PLAYER2, PLAYER1, PLAYER1],
+    ],
+    PLAYER1
+  )
+);
+
+console.log(
+  isWinner(
+    [
+      [PLAYER1, PLAYER2, PLAYER1],
+      [PLAYER1, PLAYER2, PLAYER2],
+      [PLAYER2, PLAYER1, PLAYER1],
+    ],
+    PLAYER2
   )
 );
