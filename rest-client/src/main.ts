b05433bb1403933d7da4a4187fed0205541f16ce
Brian Chirgwin <bchirgwin42@gmail.com>
2024-03-07 15:55:00 -0600

    create rest server

--- rest-client/src/main.ts
+++ rest-client/src/main.ts
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 export function handleGetRequest() {
-  axios.defaults.baseURL = 'https://swapi.dev/api';
+  axios.defaults.baseURL = 'http://localhost:3000/api';
 
   axios.get('/people/1').then((res: any) => {
     console.log(res.data);
---
+++ rest-server/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import { people } from './people';
+
+dotenv.config();
+
+const app: Express = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.get('/api/people/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  const person = people[id];
+
+  res.send(person);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
